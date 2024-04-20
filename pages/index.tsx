73dde0561c35772728fdba97261a7b6f96e19558
leon <leonlenk@gmail.com>
2024-04-19 21:08:40 -0700

    Added welcome page graphics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,29 @@
+import styles from "@/styles/index.module.scss";
+
 export default function Page() {
-	return <></>;
+	return (
+		<>
+			<div className={styles.welcomeContainer}>
+				<div className={styles.welcomeTextContainer}>
+					<h1>Welcome to</h1>
+					<div className={styles.logoContainer}>
+						<span className={styles.aiIcon} />
+						<p className={styles.acmText}>acm.</p>
+						<p className={styles.aiText}>ai</p>
+						<p className={styles.bangText}>!</p>
+					</div>
+					<h4>
+						Leon puts the L in
+						<br />
+						Machine Leonrning
+						<br />
+						:)
+					</h4>
+				</div>
+				<div className={styles.diamondBackground}>
+					<div className={styles.diamond}></div>
+				</div>
+			</div>
+		</>
+	);
 }
