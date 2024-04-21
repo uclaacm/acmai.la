ad7599f2bc3f037610aa1025b004ab9f623734b0
leon <leonlenk@gmail.com>
2024-04-21 03:57:32 -0700

    Added page scrolling for the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,17 +32,47 @@ export default function Page() {
 			{/* page two */}
 			<div className={styles.stackContainer}>
 				<div className={styles.windowContainer}>
-					<div className={styles.whatDoWeDo}>
-						<p>hasdfjiasjfdiajsiofdjioi</p>
+					<div className={styles.pageTwoContainer}>
+						<h1 className={styles.bigText}>Learn about AI!</h1>
 					</div>
 				</div>
 			</div>
 
+			{/* page three */}
 			<div className={styles.stackContainer}>
 				<div className={styles.windowContainer}>
-					<div className={styles.next}>
-						<p>hasdfjiasjfioi </p>
+					<div className={styles.pageThreeContainer}>
+						<h1 className={styles.bigText}>
+							Build your own models from scratch!
+						</h1>
 					</div>
+				</div>
+			</div>
+
+			{/* page four */}
+			<div className={styles.stackContainer}>
+				<div className={styles.windowContainer}>
+					<div className={styles.pageFourContainer}>
+						<h1 className={styles.bigText}>Join Projects!</h1>
+					</div>
+				</div>
+			</div>
+
+			{/* triangle page */}
+			<div className={styles.triangleContainer}>
+				<div className={styles.triangleContent}>
+					<h1 className={styles.triangleTitle}>Who We Are</h1>
+					<p className={styles.triangleText}>
+						maxine wu (she/her) — 04/12/2024 1:06 PM @everyone Hope everyone is
+						having a wonderful Friday :) Quick reminder that AI Spring 2024
+						Officer Apps are due tonight 11:59pm:
+						https://tinyurl.com/spr24aiofficerapps :) We strongly encourage you
+						to apply if you are passionate about the ACM AI community and want
+						to become more involved; feel free to ask any of the ACM AI officers
+						if you have any questions. Plug to also apply for AI Spring 2024
+						Project Applications if you are at all interested, and feel free to
+						ask in ⁠🧠projects-general : https://bit.ly/proj_app :)
+					</p>
 				</div>
 			</div>
 		</>
