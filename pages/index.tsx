3685b0dbe285fd323d452717cddde2c3dc488a53
leon <leonlenk@gmail.com>
2024-04-20 19:51:30 -0700

    added cool scrolling effects on the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,46 @@ import styles from "@/styles/index.module.scss";
 export default function Page() {
 	return (
 		<>
-			<div className={styles.welcomeContainer}>
-				<div className={styles.diamondBackground}>
-					<div className={styles.diamond}></div>
-				</div>
-				<div className={styles.welcomeTextContainer}>
-					<h1>Welcome to</h1>
-					<div className={styles.logoContainer}>
-						<p className={styles.acmText}>acm.</p>
-						<p className={styles.aiText}>ai</p>
-						<p className={styles.bangText}>!</p>
+			{/* page one */}
+			<div className={styles.stackContainer}>
+				<div className={styles.windowContainer}>
+					<div className={styles.welcomeContainer}>
+						<div className={styles.diamondBackground}>
+							<div className={styles.diamond}></div>
+						</div>
+						<div className={styles.welcomeTextContainer}>
+							<h1>Welcome to</h1>
+							<div className={styles.logoContainer}>
+								<p className={styles.acmText}>acm.</p>
+								<p className={styles.aiText}>ai</p>
+								<p className={styles.bangText}>!</p>
+							</div>
+							<h4>
+								Leon puts the L in
+								<br />
+								Machine Leonrning
+								<br />
+								:)
+							</h4>
+						</div>
 					</div>
-					<h4>
-						Leon puts the L in
-						<br />
-						Machine Leonrning
-						<br />
-						:)
-					</h4>
+				</div>
+			</div>
+
+			{/* page two */}
+			<div className={styles.stackContainer}>
+				<div className={styles.windowContainer}>
+					<div className={styles.whatDoWeDo}>
+						<p>hasdfjiasjfdiajsiofdjioi</p>
+					</div>
+				</div>
+			</div>
+
+			<div className={styles.stackContainer}>
+				<div className={styles.windowContainer}>
+					<div className={styles.next}>
+						<p>hasdfjiasjfioi </p>
+					</div>
 				</div>
 			</div>
 		</>
