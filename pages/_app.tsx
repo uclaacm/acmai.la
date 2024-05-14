c09d57078a7246da0ad17c7739b076b963a5826f
leon <leonlenk@gmail.com>
2024-05-13 23:12:36 -0700

    remove old metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 import type { AppProps } from "next/app";
-import type { Metadata } from "next";
 import Head from "next/head";
 
 import PageWrapper from "@/components/pageWrapper";
 
 import "../styles/globals.scss";
-
-export const metadata: Metadata = {
-	title: "acm ai website",
-};
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
